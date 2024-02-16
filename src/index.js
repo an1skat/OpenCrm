3e2228de3dbe25b19529501e0f1839f1fdec311e
an1skat <156462349+an1skat@users.noreply.github.com>
2024-02-16 21:08:26 +0200

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router basename="/OpenCrmm">
+    <Router basename="/OpenCrm">
       <App />
     </Router>
   </React.StrictMode>
