6493a53489c9636fba421aa4bbe0c6287bae9bb8
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2024-02-18 12:55:59 +0200

    add nav

---
+++ src/nav.js
@@ -0,0 +1,31 @@
+const Home = () => {
+    return (
+    <p>OpenCrm</p>
+        <nav>
+          <ul>
+           <li><a>Пользователи</a>
+            </li> 
+                    <li>
+                        <a>Группы компаний</a>
+            </li> 
+                    <li>
+                        <a>Контрагенты</a>
+            </li> 
+                    <li>
+                        <a>Контракты</a>
+            </li> 
+                  <li>
+                        <a>Сделки</a>
+            </li>   
+                    <li>
+                        <a>Сделки</a>
+            </li>  
+                    <li>
+                        <a>Настройки</a>
+            </li>  
+            </ul>  
+      </nav>
+  );
+};
+
+export default Home;
