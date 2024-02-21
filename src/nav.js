cdacdc62d7b34d6097917c913964c0e230cdc56c
vladyslava-martyniuk <vlada.martyniuk@gmail.com>
2024-02-21 18:04:19 +0200

    add new page

--- src/nav.js
+++ src/nav.js
@@ -1,31 +1,30 @@
-const Home = () => {
+const navbar= () => {
     return (
-    <p>OpenCrm</p>
-        <nav>
-          <ul>
-           <li><a>Пользователи</a>
-            </li> 
-                    <li>
-                        <a>Группы компаний</a>
-            </li> 
-                    <li>
-                        <a>Контрагенты</a>
-            </li> 
-                    <li>
-                        <a>Контракты</a>
-            </li> 
-                  <li>
-                        <a>Сделки</a>
-            </li>   
-                    <li>
-                        <a>Сделки</a>
-            </li>  
-                    <li>
-                        <a>Настройки</a>
-            </li>  
-            </ul>  
-      </nav>
+    <><p>OpenCrm</p><nav>
+            <ul>
+                <li><a>Пользователи</a>
+                </li>
+                <li>
+                    <a>Группы компаний</a>
+                </li>
+                <li>
+                    <a>Контрагенты</a>
+                </li>
+                <li>
+                    <a>Контракты</a>
+                </li>
+                <li>
+                    <a>Сделки</a>
+                </li>
+                <li>
+                    <a>Сделки</a>
+                </li>
+                <li>
+                    <a>Настройки</a>
+                </li>
+            </ul>
+        </nav></>
   );
 };
 
-export default Home;
+export default navbar;
